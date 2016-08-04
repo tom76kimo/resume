0ad30fea3a849afa463c34b73e377773df20e42e
Tom Wu <tom76kimo@yahoo-inc.com>
2016-08-04 17:45:46 +0800

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,18 @@
 <template>
-
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
-
+    <header-banner></header-banner>
     <router-view>
     </router-view>
 </template>
 
 <script>
 import Hello from './components/Hello';
+import HeaderBanner from './components/HeaderBanner';
 
 export default {
   replace: false,
   components: {
     Hello,
+    HeaderBanner,
   },
 };
 </script>
@@ -38,30 +20,11 @@ export default {
 <style>
 html {
   height: 100%;
+  margin: 0;
 }
-
 body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
+  margin: 0;
+  background-color: #F5F5F5;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
 }
 </style>
---
+++ src/components/HeaderBanner.vue
@@ -0,0 +1,74 @@
+<template>
+<div class="banner">
+  <div class="logo">tom76kimo</div>
+  <ul>
+    <li v-for="classEntry in classes" v-on:click="">{{classEntry}}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      classes: [
+        'Home',
+        'About',
+        'Project',
+        'Skill',
+        'Experience',
+        'Education',
+        'Contact',
+      ],
+    };
+  },
+  methods() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+.banner {
+  background-color: #030303;
+  display: flex;
+  flex-direction: row;
+}
+.logo {
+  color: white;
+  flex: 3;
+  line-height: 70px;
+  padding-left: 20px;
+  box-sizing: border-box;
+  font-size: 28px;
+  font-weight: 700;
+}
+ul {
+  height: 100%;
+  margin: 0;
+  display: flex;
+  flex-direction: row;
+  flex: 4;
+}
+li {
+  transition: all 1s ease;
+  color: white;
+  height: 70px;
+  flex: 1;
+  list-style-type: none;
+  text-align: center;
+  line-height: 70px;
+  cursor: pointer;
+  user-select: none;
+}
+
+li.avtive {
+  background: #eeeeee;
+}
+
+li:hover {
+  background: #eeeeee;
+  color: black;
+}
+</style>
