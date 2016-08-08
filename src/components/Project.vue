11d5491dd54cfb50a7f62ea226937e686cc52f5e
Tom Wu <tom76kimo@yahoo-inc.com>
2016-08-08 15:53:44 +0800

    refine template

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,27 +1,15 @@
 <template>
     <div class="container">
         <div class="title">Projects</div>
-        <div class="project-block">
+        <div v-for="project in projects" class="project-block">
             <div class="project-title">
-                <a href="https://tw.yahoo.com" target="_blank">Yahoo Front Page</a>
+                <a href="{{project.titleLink}}" target="_blank">{{project.title}}</a>
             </div>
-            <div class="duration">Jan. 2015 - Now</div>
+            <div class="duration">{{project.duration}}</div>
             <div class="project-table">
                 <div class="project-details">
-                    <img class="project-avatar" src="https://s1.yimg.com/rz/d/yahoo_frontpage_zh-Hant-TW_s_f_p_bestfit_frontpage_2x.png" height="50" />
-                    <div class="project-content">The main site of Yahoo Taiwan. Work on advertise fetching and balance the loading of site. </div>
-                </div>
-            </div>
-        </div>
-        <div class="project-block">
-            <div class="project-title">
-                <a href="https://github.com/acss-io/webpack-atomizer-loader" target="_blank">webpack-atomizer-loader</a>
-            </div>
-            <div class="duration">May. 2016 - Now</div>
-            <div class="project-table">
-                <div class="project-details">
-                    <img class="project-avatar" src="https://avatars2.githubusercontent.com/u/18367805?v=3&s=200" height="50" />
-                    <div class="project-content">A webpack loader of Atomic CSS. Accepted by ACSS organization.</div>
+                    <img class="project-avatar" src="{{project.avatar}}" height="50" />
+                    <div class="project-content">{{project.content}}</div>
                 </div>
             </div>
         </div>
@@ -29,6 +17,29 @@
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: 'Yahoo Front Page',
+          titleLink: 'https://tw.yahoo.com',
+          duration: 'Jan. 2015 - Now',
+          avatar: 'https://s1.yimg.com/rz/d/yahoo_frontpage_zh-Hant-TW_s_f_p_bestfit_frontpage_2x.png',
+          content: 'The main site of Yahoo Taiwan. ' +
+           'Work on advertise fetching and balance the loading of site.',
+        },
+        {
+          title: 'webpack-atomizer-loader',
+          titleLink: 'https://github.com/acss-io/webpack-atomizer-loader',
+          duration: 'May. 2016 - Now',
+          avatar: 'https://avatars2.githubusercontent.com/u/18367805?v=3&s=200',
+          content: 'A webpack loader of Atomic CSS. Accepted by ACSS organization.',
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
